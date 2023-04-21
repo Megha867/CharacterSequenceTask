0916eebe14b5207e2aba121d13f335232b050a79
Megha <megha.a.karampuri@boeing.com>
2023-04-21 21:44:52 +0530

    test case for GetSequenceCharacterFromOneToOddNumber

--- src/test/java/com/numbertochar/NumberToCharacterTest.java
+++ src/test/java/com/numbertochar/NumberToCharacterTest.java
@@ -27,5 +27,11 @@ public class NumberToCharacterTest {
 		String sequenceCharacter= convert.getCharacterSequence(2);
 		assertEquals("bA", sequenceCharacter);
 	}
+	
+	@Test
+	public void testGetSequenceCharacterFromOneToOddNumber() {
+		String sequenceCharacter= convert.getCharacterSequence(3);
+		assertEquals("bAb", sequenceCharacter);
+	}
 
 }
